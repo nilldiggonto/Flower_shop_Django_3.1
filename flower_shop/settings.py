620b098bd7233a8c53dc5aa3a629324f0d628478
nilldiggonto <nilldiggonto@gmail.com>
2020-11-13 00:59:31 +0600

    Reverse Url

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_NAME = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -47,7 +47,7 @@ ROOT_URLCONF = 'flower_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_NAME,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,3 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+## MEDIA
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import product_list,product_detail
+
+urlpatterns = [
+    path('',product_list,name='product_list'),
+    path('product/<slug:category_slug>/',product_list,name='product_list_cat'),
+    path('product/<int:id>/<slug:slug>/',product_detail,name= 'product_detail'),
+
+]
