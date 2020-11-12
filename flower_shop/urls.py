75b4f657ad530c61a6d711271fbe5c7b21ccb48a
nilldiggonto <nilldiggonto@gmail.com>
2020-11-13 02:47:22 +0600

    Cart Urls

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -5,5 +5,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/',include('cart.urls',namespace='cart')),
     path('',include('shop.urls', namespace='shop')),
+    
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
