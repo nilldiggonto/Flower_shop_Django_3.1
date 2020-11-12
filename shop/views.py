95b2816f7e343d283a6b7dcdda0a5af7ec0870ab
nilldiggonto <nilldiggonto@gmail.com>
2020-11-13 00:47:00 +0600

    Product View

--- shop/views.py
+++ shop/views.py
@@ -21,3 +21,15 @@ def product_list(request,category_slug=None):
     }
     return render(request,template_name,context)
 
+
+### Product Detail
+def product_detail(request,id,slug):
+    product = get_object_or_404(Product,id=id,slug=slug,available=True)
+
+    template_name = 'shop/product/detail.html'
+
+    context = {
+        'product':product,
+    }
+    return render(request,template_name,context)
+
