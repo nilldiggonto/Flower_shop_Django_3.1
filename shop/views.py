cb7b67273e6cfb316f1c07a2ae3bbefc8b801172
nilldiggonto <nilldiggonto@gmail.com>
2020-11-13 01:12:58 +0600

    Product List

--- shop/views.py
+++ shop/views.py
@@ -10,7 +10,7 @@ def product_list(request,category_slug=None):
     products = Product.objects.filter(available=True)
 
     if category_slug:
-        categories = get_object_or_404(Category,slug= category_slug)
+        category = get_object_or_404(Category,slug= category_slug)
         products = Product.filter(category=category)
     
     template_name = 'shop/product/list.html'
